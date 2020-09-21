cb52c5395c0028bb8ca38364d2430bd448393c12
Jessica Smith <jess@mintopia.net>
2020-09-21 23:14:22 +0200

    Initial work

---
+++ certbot_dns_constellix/dns_constellix.py
@@ -0,0 +1,264 @@
+"""DNS Authenticator for ISPConfig."""
+import json
+import logging
+import time
+
+import requests
+import zope.interface
+
+from certbot import errors
+from certbot import interfaces
+from certbot.plugins import dns_common
+
+logger = logging.getLogger(__name__)
+
+
+@zope.interface.implementer(interfaces.IAuthenticator)
+@zope.interface.provider(interfaces.IPluginFactory)
+class Authenticator(dns_common.DNSAuthenticator):
+    """DNS Authenticator for Constellix
+
+    This Authenticator uses the Constellix DNS REST API to fulfill a dns-01 challenge.
+    """
+
+    description = "Obtain certificates using a DNS TXT record (if you are using Constellix for DNS)."
+    ttl = 60
+
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
+
+    @classmethod
+    def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
+        super(Authenticator, cls).add_parser_arguments(
+            add, default_propagation_seconds=120
+        )
+        add("credentials", help="Constellix credentials INI file.")
+
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS TXT record to respond to a dns-01 challenge using "
+            + "the Constellix DNS REST API."
+        )
+
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            "credentials",
+            "Constellix DNS API credentials INI file",
+            {
+                "endpoint": "API endpoint for Constellix DNS API",
+                "apikey": "API Key for Constellix DNS API.",
+                "secretkey": "Secret Key for Constellix DNS API.",
+            },
+        )
+
+    def _perform(self, domain, validation_name, validation):
+        self._get_constellix_client().add_txt_record(
+            domain, validation_name, validation, self.ttl
+        )
+
+    def _cleanup(self, domain, validation_name, validation):
+        self._get_constellix_client().del_txt_record(
+            domain, validation_name, validation, self.ttl
+        )
+
+    def _get_constellix_client(self):
+        return _ConstellixClient(
+            self.credentials.conf("endpoint"),
+            self.credentials.conf("apikey"),
+            self.credentials.conf("secretkey"),
+        )
+
+
+class _ConstellixClient(object):
+    """
+    Encapsulates all communication with the Constellix DNS REST API.
+    """
+
+    def __init__(self, endpoint, username, password):
+        logger.debug("creating constellixclient")
+        self.endpoint = endpoint
+        self.apikey = apikey
+        self.secretkey = secretkey
+        self.session = requests.Session()
+        self.session.headers.update({'x-cnsdns-apiKey': self.apikey})
+
+    def _current_time(self):
+        return str(int(time.time() * 1000))
+
+    def _hmac_hash(self, now):
+        return hmac.new(self.secretkey.encode('utf-8'), now.encode('utf-8'),
+                        digestmod=hashlib.sha1).digest()
+
+    def _updatesecurityheaders(self):
+        logger.debug("generating securitytoken for request")
+        now = self._current_time()
+        hmac_hash = self._hmac_hash(now)
+        session.headers.update({
+            'x-cnsdns-hmac': b64encode(hmac_hash),
+            'x-cnsdns-requestDate': now
+        })
+
+    def _api_request(self, method, action, data):
+        url = self._get_url(action)
+        self._updatesecurityheaders()
+        resp = self.session.request(method, url, json=data)
+        logger.debug("API Request to URL: %s", url)
+        if resp.status_code != 200:
+            raise errors.PluginError(
+                "HTTP Error during login {0}".format(resp.status_code)
+            )
+        try:
+            result = resp.json()
+        except:
+            raise errors.PluginError(
+                "API response with non JSON: {0}".format(resp.text)
+            )
+        if result["code"] == "ok":
+            return result["response"]
+        elif result["code"] == "remote_fault":
+            raise errors.PluginError(
+                "API response with an error: {0}".format(result["message"])
+            )
+        else:
+            raise errors.PluginError("API response unknown {0}".format(resp.text))
+
+    def _get_url(self, action):
+        return "{0}/{1}".format(self.endpoint, action)
+
+    def _get_server_id(self, zone_id):
+        zone = self._api_request("dns_zone_get", {"primary_id": zone_id})
+        return zone["server_id"]
+
+    def add_txt_record(self, domain, record_name, record_content, record_ttl):
+        """
+        Add a TXT record using the supplied information.
+
+        :param str domain: The domain to use to look up the managed zone.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+        :param str record_content: The record content (typically the challenge validation).
+        :param int record_ttl: The record TTL (number of seconds that the record may be cached).
+        :raises certbot.errors.PluginError: if an error occurs communicating with the Constellix DNS API
+        """
+        zone_id, zone_name = self._find_managed_zone_id(domain, record_name)
+        if zone_id is None:
+            raise errors.PluginError("Domain not known")
+        logger.debug("domain found: %s with id: %s", zone_name, zone_id)
+        o_record_name = record_name
+        record_name = record_name.replace(zone_name, "")[:-1]
+        logger.debug(
+            "using record_name: %s from original: %s", record_name, o_record_name
+        )
+        record = self.get_existing_txt(zone_id, record_name, record_content)
+        if record is not None:
+            if record["data"] == record_content:
+                logger.info("already there, id {0}".format(record["id"]))
+                return
+            else:
+                logger.info("update {0}".format(record["id"]))
+                self._update_txt_record(
+                    zone_id, record["id"], record_name, record_content, record_ttl
+                )
+        else:
+            logger.info("insert new txt record")
+            self._insert_txt_record(zone_id, record_name, record_content, record_ttl)
+
+    def del_txt_record(self, domain, record_name, record_content, record_ttl):
+        """
+        Delete a TXT record using the supplied information.
+
+        :param str domain: The domain to use to look up the managed zone.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+        :param str record_content: The record content (typically the challenge validation).
+        :param int record_ttl: The record TTL (number of seconds that the record may be cached).
+        :raises certbot.errors.PluginError: if an error occurs communicating with the Constellix DNS API
+        """
+        zone_id, zone_name = self._find_managed_zone_id(domain, record_name)
+        if zone_id is None:
+            raise errors.PluginError("Domain not known")
+        logger.debug("domain found: %s with id: %s", zone_name, zone_id)
+        o_record_name = record_name
+        record_name = record_name.replace(zone_name, "")[:-1]
+        logger.debug(
+            "using record_name: %s from original: %s", record_name, o_record_name
+        )
+        record = self.get_existing_txt(zone_id, record_name, record_content)
+        if record is not None:
+            if record["data"] == record_content:
+                logger.debug("delete TXT record: %s", record["id"])
+                self._delete_txt_record(zone_id, record["id"])
+
+    def _prepare_data(self, record_name, record_content, record_ttl):
+        return {
+            "name": record_name,
+            "ttl": record_ttl,
+            "roundRobin": [
+                {
+                    "value": record_content
+                }
+            ]
+        }
+
+    def _insert_txt_record(self, zone_id, record_name, record_content, record_ttl):
+        data = self._prepare_data(record_name, record_content, record_ttl)
+        logger.debug("insert with data: %s", data)
+        result = self._api_request("POST", "domains/{0}/records/txt".format(zone_id), data)
+
+    def _update_txt_record(
+        self, zone_id, primary_id, record_name, record_content, record_ttl
+    ):
+        data = self._prepare_data(record_name, record_content, record_ttl)
+        data["primary_id"] = primary_id
+        logger.debug("update with data: %s", data)
+        result = self._api_request("PUT", "domains/{0}/records/txt/{1}".format(zone_id, record_id), data)
+
+    def _delete_txt_record(self, zone_id, record_id):
+        logger.debug("delete record id: %s", record_id)
+        result = self._api_request("DELETE", "domains/{0}/records/txt/{1}".format(zone_id, record_id)
+
+    def _find_managed_zone_id(self, domain, record_name):
+        """
+        Find the managed zone for a given domain.
+
+        :param str domain: The domain for which to find the managed zone.
+        :returns: The ID of the managed zone, if found.
+        :rtype: str
+        :raises certbot.errors.PluginError: if the managed zone cannot be found.
+        """
+
+        zone_dns_name_guesses = [record_name] + dns_common.base_domain_name_guesses(domain)
+
+        for zone_name in zone_dns_name_guesses:
+            # get the zone id
+            try:
+                logger.debug("looking for zone: %s", zone_name)
+                result = self._api_request("GET", "domains/search?exact={0}".format(zone_name))
+                if len(result) > 0:
+                    return result[0].id, result[0].name
+            except errors.PluginError as e:
+                pass
+        return None
+
+    def get_existing_txt(self, zone_id, record_name, record_content):
+        """
+        Get existing TXT records for the record name.
+
+        If an error occurs while requesting the record set, it is suppressed
+        and None is returned.
+
+        :param str zone_id: The ID of the managed zone.
+        :param str record_name: The record name (typically beginning with '_acme-challenge.').
+
+        :returns: TXT record value or None
+        :rtype: `string` or `None`
+
+        """
+        records = self._api_request("GET", "domains/{0}}/records/txt/search?exact={1}".format(zone_id, record_name))
+        for record in records:
+            data = self._api_request("GET", "domains/{0}/records/txt/{1}".format(zone_id, record.id))
+            fullvalue = ""
+            for value in data.value:
+                fullvalue += value.value
+            if fullvalue == record_content:
+                return data
+        return None
---
+++ setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = "0.2.0"
+
+install_requires = [
+    "acme>=0.29.0",
+    "certbot>=0.34.0",
+    "setuptools",
+    "requests",
+    "mock",
+    "requests-mock",
+]
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="certbot-dns-constellix",
+    version=version,
+    description="Constellix DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mintopia/certbot-dns-constellix",
+    author="Jessica Smith",
+    author_email="jsmith@constellix.com",
+    license="Apache License 2.0",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Security",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "certbot.plugins": [
+            "dns-constellix = certbot_dns_constellix.dns_constellix:Authenticator"
+        ]
+    },
+    test_suite="certbot_dns_constellix",
+)
